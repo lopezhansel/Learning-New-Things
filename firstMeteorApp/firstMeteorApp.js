956aaff37e0cfa379b23358fb111d03691c3304b
Hansel <lopezhansel@gmail.com>
2015-10-20 14:40:52 -0600

    minor changes

--- firstMeteorApp/firstMeteorApp.js
+++ firstMeteorApp/firstMeteorApp.js
@@ -1,21 +1,14 @@
- if (Meteor.isClient) {
+var Resolutions = new Mongo.Collection("resolution");
 
-  Template.body.helpers({
-    resolutions: [
-	    {title:"hello resolution #1"},
-	    {title:"bye resolution #2"},
-	    {title:"Angular is also Awesome"}
-    ]
-  });
+if (Meteor.isClient) {
+
+	Template.body.helpers({
+		resolutions: function  () {
+			return Resolutions.find();
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-  });
+	Meteor.startup(function() {});
 }
-// you can npm init to intall your node modules not in your 
-// npm install -production
-// npm install gulp --save-dev
-// angular wont uglify properly if you dont have dependancy injections
-// cd into firstMeteorApp 
-// meteor
