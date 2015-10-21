583c2efe87ebfc23ac02210e56a7d6f17e3503e3
Hansel <lopezhansel@gmail.com>
2015-10-20 22:19:21 -0600

    Adding ability to add and remove variables

--- firstMeteorApp/firstMeteorApp.js
+++ firstMeteorApp/firstMeteorApp.js
@@ -3,8 +3,35 @@ var Resolutions = new Mongo.Collection("resolution");
 if (Meteor.isClient) {
 
 	Template.body.helpers({
-		resolutions: function  () {
+		resolutions: function() {
 			return Resolutions.find();
+		}
+	});
+	Template.resolution.events({
+		'click .toggle-checked': function() {
+			Resolutions.update(this._id, {
+				$set: {
+					checked: !this.checked
+				}
+			});
+		},
+		'click .delete': function() {
+			console.log(this);
+			Resolutions.remove(this._id);
+		}
+	});
+
+	Template.body.events({
+		'submit .new-resolution': function(event) {
+			var title = event.target.title.value;
+			Resolutions.insert({
+				title: title,
+				createdAt: new Date()
+			});
+			event.target.title.value = '';
+
+			//  In order for the browser not to refresh
+			return false;
 		}
 	});
 }
