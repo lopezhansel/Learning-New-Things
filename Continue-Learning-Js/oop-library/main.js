840829ce5aea521fb363616477b1e3fdcb55ee46
Hansel <lopezhansel@gmail.com>
2015-11-21 20:37:17 -0700

    Enabling Inheritance to instances

--- Continue-Learning-Js/oop-library/main.js
+++ Continue-Learning-Js/oop-library/main.js
@@ -1,12 +1,19 @@
 // Problem :  Create a modular library using OOP
 ;(function(){
-	var root  = global || window;
+
+	// This is from underscore.js
+	// Establish the root object, `window` (`self`) in the browser, `global`
+	// on the server, or `this` in some virtual machines. We use `self`
+	// instead of `window` for `WebWorker` support.
+	 var root = typeof self == 'object' && self.self === self && self ||
+            typeof global == 'object' && global.global === global && global ||
+            this;
+
 	// Inspired from jQuery to create Objects
-
-
 	var h = function(firstName,lastName,lat,lng){
 		return new h.init(firstName,lastName,lat,lng);
 	};
+
     // Here is the function constructor allowing me to not use  the"new" keyword
     h.init = function(firstName, lastName, lat,lng) {
         
@@ -19,8 +26,20 @@
         
     };
 
+    h.prototype = {
+    	moveUp : function (){
+    		this.lat = this.lat +  1;
+    		return this;
+    	}
+    };
+
+    h.init.prototype = h.prototype;
+
     root.h = h;
 })();
 
 var person1 = h("hasnel","lopez",40,40);
+
+var hello = person1.moveUp();
+
 console.log(person1);
